8a5e5249e89413ef6cbf1983020e4f2a9ab4c155
Lily <lmayhenderson@yahoo.com>
2024-04-18 15:06:55 -0700

    Create component as a container for all PhotoListItems.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,10 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 const App = () => {
-
-  const sampleDataForPhotoListItem = {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  };
   
   return (
     <div className="App">
        {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-       <PhotoListItem data={sampleDataForPhotoListItem} />
     </div>
   );
 };
