4eeddab5b62f3a627bb3db7330f81cbe605f4a8a
Lily <lmayhenderson@yahoo.com>
2024-04-17 17:59:13 -0700

    Change tags in PhotoListItem.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,14 +4,14 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ data }) => {
   return (
-    <ul className="photo-list__item" id={data.id}>
+    <div className="photo-list__item" id={data.id}>
       <img className="photo-list__image" src={data.imageSource} />
       <div className="photo-list__user-details">
-      <img className="photo-list__user-profile" src={data.profile} />
-      <p className="photo-list__user-info">{data.username}</p>
-      <p className="photo-list__user-location" > {data.location.city}, {data.location.country}</p>
+        <img className="photo-list__user-profile" src={data.profile} />
+        <p>{data.username}</p>
+        <p className="photo-list__user-location" > {data.location.city}, {data.location.country}</p>
       </div>
-    </ul>
+    </div>
   );
 };
 
