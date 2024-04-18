376d9d1b3b49fe86de6bb6cddff73da637c1ae5f
Lily <lmayhenderson@yahoo.com>
2024-04-18 16:18:55 -0700

    Render PhotoList.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoListItem'
 import './App.scss';
 
 const App = () => {
   
   return (
     <div className="App">
-       {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <PhotoList />
     </div>
   );
 };
