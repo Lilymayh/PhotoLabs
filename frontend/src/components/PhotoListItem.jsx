76f356825cfff6568db993a8a1c0efffd71a9125
Lily <lmayhenderson@yahoo.com>
2024-04-17 17:50:27 -0700

    Add div in PhotoListItem.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,12 +4,14 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ data }) => {
   return (
-    <li className="photo-list__item" id={data.id}>
+    <ul className="photo-list__item" id={data.id}>
       <img className="photo-list__image" src={data.imageSource} />
+      <div className="photo-list__user-details">
       <img className="photo-list__user-profile" src={data.profile} />
-      <p className="photo-list__user-details" >{data.username}</p>
+      <p className="photo-list__user-info">{data.username}</p>
       <p className="photo-list__user-location" > {data.location.city}, {data.location.country}</p>
-    </li>
+      </div>
+    </ul>
   );
 };
 
