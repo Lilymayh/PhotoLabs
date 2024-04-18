840bb45ac39b89d6cb9ae5b9ab5d99275192aa3d
Lily <lmayhenderson@yahoo.com>
2024-04-17 17:19:39 -0700

    Add classNames to PhotoListItem.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,12 +4,12 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ data }) => {
   return (
-    <div id={data.id}>
-      <img src={data.imageSource} />
-      <img src={data.profile} />
-      <p>{data.username}</p>
-      <p> {data.location.city}, {data.location.country}</p>
-    </div>
+    <li className="photo-list__item" id={data.id}>
+      <img className="photo-list__image" src={data.imageSource} />
+      <img className="photo-list__user-profile" src={data.profile} />
+      <p className="photo-list__user-details" >{data.username}</p>
+      <p className="photo-list__user-location" > {data.location.city}, {data.location.country}</p>
+    </li>
   );
 };
 
