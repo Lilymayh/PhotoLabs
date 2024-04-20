9726afb6b9e19270f4f3300fcadd2c1d36441115
Lily <lmayhenderson@yahoo.com>
2024-04-19 18:23:46 -0700

    Console.log data in the modal.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoListItem from '../components/PhotoListItem'
 
-const PhotoDetailsModal = ({ onClose }) => {
+const PhotoDetailsModal = ({ data, onClose }) => {
   const handleCloseClick = () => {
     onClose(); 
   }
+
+  console.log(data)
+  
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleCloseClick}>
